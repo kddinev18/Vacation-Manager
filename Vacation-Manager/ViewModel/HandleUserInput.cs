eafb822a91e070281e90459cf2dc5c1b3bb01b62
kddinev18 <kddinev18@codingburgas.bg>
2022-10-31 22:37:31 +0200

    Implement GeneralHandler() in HandleUserInput class

--- Vacation-Manager/ViewModel/HandleUserInput.cs
+++ Vacation-Manager/ViewModel/HandleUserInput.cs
@@ -9,5 +9,18 @@ namespace DiabetesTracker.Logic
 {
     public static class HandleUserInput
     {
+        // General hander for user input
+        public static bool GeneralHandler(string userInput)
+        {
+            // If the inpt contains ', ' or '|' show error messange boc and return false
+            if (string.IsNullOrEmpty(userInput) || userInput.Contains(", ") || userInput.Contains('|'))
+            {
+                MessageBox.Show("Your input must not contain \', \' or \'|\'", "Error");
+                return false;
+            }
+
+            // Otherwise return true
+            return true;
+        }
     }
 }

4a39a3a90e94547ab65d2dc4d5c0ace5509052cd
kddinev18 <kddinev18@codingburgas.bg>
2022-10-31 20:46:11 +0200

    Set up the client application

---
+++ Vacation-Manager/View/Code behind/UserAuthenticationWindow/UserAuthenticationWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vacation_Manager.View.Code_behind.UserAuthenticationWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class UserAuthenticationWindow : Window
+    {
+        public UserAuthenticationWindow()
+        {
+            try
+            {
+                Services.SetUpConnection();
+                InitializeComponent();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("The server is currently down. Plase excuse us.", "Connection error");
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}

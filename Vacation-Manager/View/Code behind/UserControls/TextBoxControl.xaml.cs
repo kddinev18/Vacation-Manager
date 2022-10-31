092d5dd4d5531a84aedd8b793b4b37cf844b5e8e
kddinev18 <kddinev18@codingburgas.bg>
2022-10-31 21:46:33 +0200

    Import design

---
+++ Vacation-Manager/View/Code behind/UserControls/TextBoxControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vacation_Manager.Code_behind.XAML.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TextBoxControl.xaml
+    /// </summary>
+    public partial class TextBoxControl : UserControl
+    {
+        public TextBoxControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

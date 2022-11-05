9d6c8926443e22c960e62d4cd338f23631112bd1
kddinev18 <kddinev18@codingburgas.bg>
2022-11-05 18:59:47 +0200

    Add functionality for add member button

--- Vacation-Manager/View/Code behind/AddMember/AddMemberWindow.xaml.cs
+++ Vacation-Manager/View/Code behind/AddMember/AddMemberWindow.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,24 +11,34 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Vacation_Manager.View.Code_behind.AddMember;
 
-namespace Vacation_Manager.View.Code_behind.MainWindow
+namespace Vacation_Manager.View.Code_behind.AddMember
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AddMemberWindow.xaml
     /// </summary>
-    public partial class VacationManagerMainWindow : Window
+    public partial class AddMemberWindow : Window
     {
         private bool _isMaximized = false;
-        public VacationManagerMainWindow()
+        public AddMemberWindow()
         {
             InitializeComponent();
         }
 
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if(e.ChangedButton == MouseButton.Left)
+            if (e.ChangedButton == MouseButton.Left)
             {
                 this.DragMove();
             }
@@ -36,13 +46,13 @@ namespace Vacation_Manager.View.Code_behind.MainWindow
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if(e.ClickCount == 2)
+            if (e.ClickCount == 2)
             {
-                if(_isMaximized)
+                if (_isMaximized)
                 {
                     this.WindowState = WindowState.Normal;
-                    this.Width = 1080;
-                    this.Height = 720;
+                    this.Width = 350;
+                    this.Height = 350;
 
                     _isMaximized = false;
                 }
@@ -53,12 +63,6 @@ namespace Vacation_Manager.View.Code_behind.MainWindow
                     _isMaximized = true;
                 }
             }
-        }
-
-        private void AddMembersButton_Click(object sender, RoutedEventArgs e)
-        {
-            AddMemberWindow addMemberWindow = new AddMemberWindow();
-            addMemberWindow.Show();
         }
     }
 }
